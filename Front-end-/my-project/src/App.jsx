58b59e377d15db6f23556af9d5d6639184653958
Vidya Sagar.S <vsagar71004@gmail.com>
2026-05-23 20:14:05 +0530

    fixed react router deployment

--- Front-end-/my-project/src/App.jsx
+++ Front-end-/my-project/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Navbar from './COMPONENTS/Navbar'
 
 import { Routes, Route } from 'react-router-dom'
+
+import Navbar from './COMPONENTS/Navbar'
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -14,7 +15,7 @@ function App() {
 
     <div>
 
-      <Navbar/>
+      <Navbar />
 
       <Routes>
 
